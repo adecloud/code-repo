48e1a87fad350d1e88a091234f3bfee5cf55f67d
adecloud <adeotiademola012@gmail.com>
2022-08-04 18:12:10 +0100

    second commit

--- main.tf
+++ main.tf
@@ -1,23 +1,35 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {} 
 
-resource "aws_instance" "Linux_server" {
-  ami           = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
+
+
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block       = var.vpc_cidr_block
 
   tags = {
-    Name = "Linux_Instance"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id     = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
