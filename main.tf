a3f1f63b63b7cc8442834cc392acb395e27f5173
adecloud <adeotiademola012@gmail.com>
2022-09-28 14:24:43 +0100

    created security group

--- main.tf
+++ main.tf
@@ -1,16 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
 variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {} 
+variable my_ip {}
 
 
 
@@ -33,3 +29,62 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+resource "aws_route_table" "my-app-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-app-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+
+}
+
+resource "aws_internet_gateway" "my-app-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.my-app-route-table.id
+}
+
+resource "aws_security_group" "my-app-sg" {   
+  vpc_id      = aws_vpc.myapp-vpc.id
+  name = "myapp-sg"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.my_ip]
+  }
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+      Name = "${var.env_prefix}-sg"
+  }
+
+}
+  
